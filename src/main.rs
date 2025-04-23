2467db0ac5724aa30f2cf4acd0be1fac90115e2c
Ashton Sawyer <ashtonesawyer@gmail.com>
2025-04-23 13:38:31 -0700

    install dependencies (sorta) + start README

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use cortex_m_rt::entry;
+use embedded_hal::digital::InputPin;
+#[rustfmt::skip]
+use microbit::{
+    board::{Board, Buttons},
+    display::blocking::Display,
+    hal::{
+        Rng as HwRng,
+        timer::Timer,
+    },
+};
+use nanorand::{pcg64::Pcg64, Rng, SeedableRng};
+
+mod life;
+use life::*;
+
 fn main() {
     println!("Hello, world!");
 }
