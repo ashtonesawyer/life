b419c31bf90ba788050965c08e7dc0c55473110e
Ashton Sawyer <ashtonesawyer@gmail.com>
2025-04-28 13:19:30 -0700

    start life

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![no_main]
+#![no_std]
+
 use cortex_m_rt::entry;
 use embedded_hal::digital::InputPin;
 #[rustfmt::skip]
@@ -14,6 +17,25 @@ use nanorand::{pcg64::Pcg64, Rng, SeedableRng};
 mod life;
 use life::*;
 
-fn main() {
-    println!("Hello, world!");
+enum State {
+    LedOn,
+    LedOff,
+}
+
+fn randomize_board() {
+    
+}
+
+#[entry]
+fn init() -> ! {
+    let mut board = Board::take().unwrap();
+    let mut display = Display::new(board.display_pins);
+    let mut timer = Timer::new(board.TIMER0);
+    let mut btn_a = board.buttons.button_a.degrade();
+    let mut btn_b = board.buttons.button_b.degrade();
+
+    loop {
+        let mut leds = randomize_board();
+        board
+    }
 }
